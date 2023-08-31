b08b6fb84aaeb6dbd5993191e43bfd06e0aa9133
davidscodex <98758039+davidscodex@users.noreply.github.com>
2023-08-31 10:11:12 -0400

    Use EchoLayer Pipeline (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
-import { getInput } from "@actions/core";
-import { context, getOctokit } from "@actions/github";
+import { configuration, discoveryPipeline } from "echolayer-pipeline-lib";
+import { getInputConfiguration } from "./config";
+import { createPR } from "./git";
+import { setFailed } from "@actions/core";
 
-type GithubContext = typeof context;
-
-const inputName = getInput("name");
-
-greet(inputName, getRepoUrl(context));
-
-function greet(name: string, repoUrl: string) {
-  console.log(`'Hello ${name}! You are running a GH Action in ${repoUrl}'`);
+async function runAction(){
+	const options: configuration = getInputConfiguration();
+	try {
+		await discoveryPipeline(options);
+	} catch (e) {
+		console.error(e);
+		setFailed("Error running EchoLayer Pipeline import");
+		return;
+	}
+	try {
+		await createPR(options);
+	} catch (e) {
+		console.error(e);
+		setFailed("Error creating PR");
+		return;
+	}
 }
 
-function getRepoUrl({ repo, serverUrl }: GithubContext): string {
-  return `${serverUrl}/${repo.owner}/${repo.repo}`;
-}
+runAction();
