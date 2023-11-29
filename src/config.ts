fd8ce64388127cc95f3390bf36578501b549a6c9
Saumil Patel <14961025+saumilptl@users.noreply.github.com>
2023-11-29 10:16:39 -0800

    Addressing feedback adjusting configs

--- src/config.ts
+++ src/config.ts
@@ -21,12 +21,13 @@ export function getInputConfiguration(): configuration {
 	return {
 		basePath: getInputWithDefault("GITHUB_WORKSPACE", process.cwd()),
 		apiPath: getInput("apiPath", { required: true }),
-		apiKey: getInput("apiKey", { required: true }),
+		apiKey: getInputWithDefault("apiKey", "https://api.echolayer.com/api"),
 		defaultBranch: getInputWithDefault("branch", "main"),
 		remoteUrl: `${context.serverUrl}/${context.repo.owner}/${context.repo.repo}.git`,
 		source: "GitHub",
 		pullBranchName: getInputWithDefault("pullBranchName", "EchoLayerPipeline"),
 		commitPrefix: getInputWithDefault("commitPrefix", "*chore*"),
+		daysToAnalyze: Number(getInputWithDefault("daysToAnalyze", "365")),
 		modules: moduleOptions
 	}
 }
